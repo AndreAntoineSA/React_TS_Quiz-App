5b4922cf05459d7ce35634a1db48758cfeb1d7be
andre <andreantoine0016@gmail.com>
2021-04-21 19:49:44 +0200

    added tailwind styles

--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -10,12 +10,12 @@ type Props = {
 }
 
 const QuestionCard: React.FC<Props> = ({ question, answers, callback, userAnswer, questionNr, totalQuestions }) =>
-(<div>
-    <p>Question: {questionNr}/{totalQuestions}</p>
-    <p dangerouslySetInnerHTML={{ __html: question }} />
-    <div>
+(<div className="flex flex-col items-center">
+    <p className="italic text-sm">Question: {questionNr}/{totalQuestions}</p>
+    <p className="mt-5 bold text-lg" dangerouslySetInnerHTML={{ __html: question }} />
+    <div className="mt-5 grid grid-flow-col grid-cols-2 grid-rows-2 gap-4">
         {answers.map(answer => (<div key={answer}>
-            <button disabled={!!userAnswer} value={answer} onClick={callback}><span dangerouslySetInnerHTML={{ __html: answer }} /></button>
+            <button className="text-md border m-1 p-2 w-full bg-white hover:bg-gray-100 focus:outline-none rounded" disabled={!!userAnswer} value={answer} onClick={callback}><span dangerouslySetInnerHTML={{ __html: answer }} /></button>
         </div>))}
     </div>
 </div>)
